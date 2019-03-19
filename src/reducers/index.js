b1e43f010982b2aafe100525c069b1f1fd6cdbae
bwadlow <bwadlow@gmail.com>
2019-03-19 15:33:40 -0400

    data fetched and displaying

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
 
-export default combineReducers({})
+export default combineReducers({
+  posts:postsReducer
+});
