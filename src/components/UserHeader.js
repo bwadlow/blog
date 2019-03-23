a31bfecca2557584d68e7d6e61a88db6b63f1958
bwadlow <bwadlow@gmail.com>
2019-03-23 18:06:23 -0400

    refactor action creators to avoid overfetching

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,11 @@
 import React,{ Component} from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions'
+
 
 
 class UserHeader extends Component {
 
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId)
-  }
+
 
   render() {
     const user=this.props.users.find(user => user.id === this.props.userId)
@@ -22,4 +20,4 @@ const mapStateToProps =state => {
   return {users:state.users}
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader)
+export default connect(mapStateToProps)(UserHeader)
